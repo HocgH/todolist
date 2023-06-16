95d7bddffe5eb4c000dc144ea4004d64d5fd8774
HocgH <57635765+HocgH@users.noreply.github.com>
2023-06-16 14:47:08 +0300

    initial commit
    initial commit come with installing reactJs, Tailwindcss and some first few things from the layout

---
+++ todolist-pj/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {AiOutlinePlus} from 'react-icons/ai'
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#E1AEFF] to-[#FFECEC]`
+}
+
+function App() {
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
